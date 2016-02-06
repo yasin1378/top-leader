ffd55f07dd13c15af99be9f57ae2e42538b3171a
yasin1378 <yasinbayad1378@gmail.com>
2016-02-06 23:20:14 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {134 436 141,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -239,7 +239,7 @@ Admins
 @imandaneshi [Developer]
 @Rondoozle [Developer]
 @seyedan25 [Manager]
-
+@yasin_bayad
 Special thanks to
 awkward_potato
 Siyanew
